e326ce7f2be5ac89cf2cc5d4daf34ecff00bb22d
keisei_1092 <keisei_1092@yahoo.co.jp>
2017-03-08 15:56:18 +0900

    Initial Commit

---
+++ LottiePractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LottiePractice
+//
+//  Created by Keisei Saito on 2017/3/8.
+//  Copyright © 2017年 keisei_1092. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
